7e32ec3f3218437ba96d74a4b92b771ca0e16696
Robert Melendez <melendez@robertdev.net>
2023-08-09 13:12:19 -0400

    edit meta

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 function page() {
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col">
+    <div className="w-full h-screen flex justify-center items-center flex-col p-4">
       <div className="flex justify-center items-center py-3 ">
-        <p className="text-3xl font-bold">Welcome to Your Deli</p>
+        <p className="text-2xl font-bold">Welcome to Your Deli</p>
       </div>
       <div>
-        <p className="py-2 text-xl">
+        <p className="py-2 text-lg">
           If you have any question or issues please contact us at:{" "}
         </p>
         <p className="text-xl text-center">
@@ -28,7 +28,7 @@ function page() {
         </Link>
       </div>
       <div className="absolute bottom-10 right-10">
-        <p>@ RobertDev {new Date().getFullYear()}</p>
+        <p className="text-sm">@ RobertDev {new Date().getFullYear()}</p>
       </div>
     </div>
   );
